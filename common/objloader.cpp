1b77715f2d726ee3b22509eba9d769ba5bde77ce
Calvin1602 <Calvin1602@localhost>
2012-02-03 22:20:24 +0100

    * Moved TBN computation from fragment to shader; better code matchin with tutorial + Comment about LightColor and LightPower in all fragment shaders + Better error message in objloader.cpp

--- common/objloader.cpp
+++ common/objloader.cpp
@@ -24,7 +24,7 @@ bool loadOBJ(
 
 	FILE * file = fopen(path, "r");
 	if( file == NULL ){
-		printf("Impossible to open the file !\n");
+		printf("Impossible to open the file ! Are you in the right path ? See Tutorial 1 for details\n");
 		return false;
 	}
 
