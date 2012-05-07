dc004c64107c97cdd01b6be96d581fb69e23f1a1
Calvin1602 <Calvin1602@localhost>
2012-05-07 20:33:35 +0200

    * Improved error message

--- tutorial04_colored_cube/tutorial04.cpp
+++ tutorial04_colored_cube/tutorial04.cpp
@@ -32,7 +32,7 @@ int main( void )
     // Open a window and create its OpenGL context
     if( !glfwOpenWindow( 1024, 768, 0,0,0,0, 32,0, GLFW_WINDOW ) )
     {
-        fprintf( stderr, "Failed to open GLFW window\n" );
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         glfwTerminate();
         return -1;
     }
