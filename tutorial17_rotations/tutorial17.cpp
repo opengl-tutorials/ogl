587013fb2bd7c0f55a5e929f166f236909ee2491
Calvin1602 <Calvin1602@localhost>
2012-12-20 19:49:32 +0100

    + New version of tutorial 17, with quaternion-based lookat & RotateTowards

--- tutorial17_rotations/tutorial17.cpp
+++ tutorial17_rotations/tutorial17.cpp
@@ -2,49 +2,181 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-
+ 
 // Include GLEW
 #include <GL/glew.h>
-
+ 
 // Include GLFW
 #include <GL/glfw.h>
-
+ 
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 #include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/norm.hpp>
 using namespace glm;
-
+ 
 // Include AntTweakBar
 #include <AntTweakBar.h>
-
-
+ 
+ 
 #include <common/shader.hpp>
 #include <common/texture.hpp>
 #include <common/controls.hpp>
 #include <common/objloader.hpp>
 #include <common/vboindexer.hpp>
-
-
-vec3 gEulerRotation;
-quat gQuaternion;
-bool gNormalizeQuaternion;
-
+ 
+ 
+vec3 gPosition1(-1.5f, 0.0f, 0.0f);
+vec3 gOrientation1;
+ 
+vec3 gPosition2( 1.5f, 0.0f, 0.0f);
+quat gOrientation2;
+ 
+bool gLookAtOther = true;
+ 
+quat RotationBetweenVectors(vec3 start, vec3 dest){
+	start = normalize(start);
+	dest = normalize(dest);
+ 
+	float cosTheta = dot(start, dest);
+	vec3 rotationAxis;
+	
+	if (cosTheta < -1 + 0.001f){
+		// special case when vectors in opposite directions :
+		// there is no "ideal" rotation axis
+		// So guess one; any will do as long as it's perpendicular to start
+		// This implementation favors a rotation around the Up axis,
+		// since it's often what you want to do.
+		rotationAxis = cross(vec3(0.0f, 0.0f, 1.0f), start);
+		if (gtx::norm::length2(rotationAxis) < 0.01 ) // bad luck, they were parallel, try again!
+			rotationAxis = cross(vec3(1.0f, 0.0f, 0.0f), start);
+		
+		rotationAxis = normalize(rotationAxis);
+		return gtx::quaternion::angleAxis(180.0f, rotationAxis);
+	}
+ 
+	// Implementation from Stan Melax's Game Programming Gems 1 article
+	rotationAxis = cross(start, dest);
+ 
+	float s = sqrt( (1+cosTheta)*2 );
+	float invs = 1 / s;
+ 
+	return quat(
+		s * 0.5f, 
+		rotationAxis.x * invs,
+		rotationAxis.y * invs,
+		rotationAxis.z * invs
+	);
+ 
+ 
+}
+ 
+ 
+quat LookAt(vec3 direction, vec3 desiredUp){
+	// Recompute desiredUp so that it's perpendicular to the direction
+	vec3 right = cross(direction, desiredUp);
+	desiredUp = cross(right, direction);
+ 
+	// Find the rotation between the front of the object (that we assume towards +Z,
+	// but this depends on your model) and the desired direction
+	quat rot1 = RotationBetweenVectors(vec3(0.0f, 0.0f, 1.0f), direction);
+	// Because of the 1rst rotation, the up is probably completely screwed up. 
+	// Find the rotation between the "up" of the rotated object, and the desired up
+	vec3 newUp = rot1 * vec3(0.0f, 1.0f, 0.0f);
+	quat rot2 = RotationBetweenVectors(newUp, desiredUp);
+ 
+	// Apply them
+	return rot2 * rot1; // remember, in reverse order.
+}
+ 
+quat operator+(quat const & q1, quat const & q2){
+	return quat(
+		q1.w + q2.w,
+		q1.x + q2.x,
+		q1.y + q2.y,
+		q1.z + q2.z
+	);
+}
+ 
+quat RotateTowards(quat q1, quat q2, float maxAngle){
+ 
+	if( maxAngle < 0.001f ){
+		// No rotation allowed. Prevent dividing by 0 later.
+		return q1;
+	}
+ 
+	float cosTheta = dot(q1, q2);
+	
+	// q1 and q2 are already equal.
+	// Force q2 just to be sure
+	if(cosTheta > 0.9999f){
+		return q2;
+	}
+ 
+	if (cosTheta < 0){
+		q1 = q1*-1.0f;
+		cosTheta *= -1.0f;
+	}
+ 
+	float angle = acos(cosTheta);
+	
+	// If there is only a 2° difference, and we are allowed 5°,
+	// then we arrived.
+	if (angle < maxAngle){
+		return q2;
+	}
+ 
+	float fT = maxAngle / angle;
+	angle = maxAngle;
+ 
+	quat res = (sin((1.0f - fT) * angle) * q1 + sin(fT * angle) * q2) / sin(angle);
+	res = normalize(res);
+	return res;
+ 
+}
+ 
 int main( void )
 {
+ 
+	glm::vec3 Xpos(+1.0f,  0.0f,  0.0f);
+	glm::vec3 Ypos( 0.0f, +1.0f,  0.0f);
+	glm::vec3 Zpos( 0.0f,  0.0f, +1.0f);
+	glm::vec3 Xneg(-1.0f,  0.0f,  0.0f);
+	glm::vec3 Yneg( 0.0f, -1.0f,  0.0f);
+	glm::vec3 Zneg( 0.0f,  0.0f, -1.0f);
+ 
+	// Testing standard, easy case
+	// Must be 90° rotation on X : 0.7 0 0 0.7
+	quat X90rot = RotationBetweenVectors(Ypos, Zpos);
+ 
+	// Testing with v1 = v2
+	// Must be identity : 0 0 0 1
+	quat id = RotationBetweenVectors(Xpos, Xpos);
+ 
+	// Testing with v1 = -v2
+	// Must be 180° on +/-Y axis : 0 +/-1 0 0
+	quat Y180rot = RotationBetweenVectors(Xpos, Xneg);
+ 
+	// Testing with v1 = -v2, but with a "bad first guess"
+	// Must be 180° on +/-Y axis : 0 +/-1 0 0
+	quat X180rot = RotationBetweenVectors(Zpos, Zneg);
+ 
+	//RotateTowards
+ 
 	// Initialise GLFW
 	if( !glfwInit() )
 	{
 		fprintf( stderr, "Failed to initialize GLFW\n" );
 		return -1;
 	}
-
+ 
 	glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
-	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
-	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 2);
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 1);
+ 
 	// Open a window and create its OpenGL context
 	if( !glfwOpenWindow( 1024, 768, 0,0,0,0, 32,0, GLFW_WINDOW ) )
 	{
@@ -52,118 +184,125 @@ int main( void )
 		glfwTerminate();
 		return -1;
 	}
-
+ 
 	// Initialize GLEW
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		return -1;
 	}
-
+ 
 	// Initialize the GUI
-	TwInit(TW_OPENGL_CORE, NULL);
+	TwInit(TW_OPENGL, NULL);
 	TwWindowSize(1024, 768);
 	TwBar * EulerGUI = TwNewBar("Euler settings");
 	TwBar * QuaternionGUI = TwNewBar("Quaternion settings");
 	TwSetParam(QuaternionGUI, NULL, "position", TW_PARAM_CSTRING, 1, "808 16");
-
-	TwAddVarRW(EulerGUI, "Euler X", TW_TYPE_FLOAT, &gEulerRotation.x, "step=0.01");
-	TwAddVarRW(EulerGUI, "Euler Y", TW_TYPE_FLOAT, &gEulerRotation.y, "step=0.01");
-	TwAddVarRW(EulerGUI, "Euler Z", TW_TYPE_FLOAT, &gEulerRotation.z, "step=0.01");
-
-	TwAddVarRW(QuaternionGUI, "Quaternion", TW_TYPE_QUAT4F, &gQuaternion, "showval=true open=true ");
-	TwAddVarRW(QuaternionGUI, "Normalize", TW_TYPE_BOOL8, &gNormalizeQuaternion, "help='Will renormalize the quaternion each frame so that the quaternion is always valid'");
-
+ 
+	TwAddVarRW(EulerGUI, "Euler X", TW_TYPE_FLOAT, &gOrientation1.x, "step=0.01");
+	TwAddVarRW(EulerGUI, "Euler Y", TW_TYPE_FLOAT, &gOrientation1.y, "step=0.01");
+	TwAddVarRW(EulerGUI, "Euler Z", TW_TYPE_FLOAT, &gOrientation1.z, "step=0.01");
+	TwAddVarRW(EulerGUI, "Pos X"  , TW_TYPE_FLOAT, &gPosition1.x, "step=0.1");
+	TwAddVarRW(EulerGUI, "Pos Y"  , TW_TYPE_FLOAT, &gPosition1.y, "step=0.1");
+	TwAddVarRW(EulerGUI, "Pos Z"  , TW_TYPE_FLOAT, &gPosition1.z, "step=0.1");
+ 
+	TwAddVarRW(QuaternionGUI, "Quaternion", TW_TYPE_QUAT4F, &gOrientation2, "showval=true open=true ");
+	TwAddVarRW(QuaternionGUI, "Use LookAt", TW_TYPE_BOOL8 , &gLookAtOther, "help='Look at the other monkey ?'");
+ 
 	// Set GLFW event callbacks. I removed glfwSetWindowSizeCallback for conciseness
 	glfwSetMouseButtonCallback((GLFWmousebuttonfun)TwEventMouseButtonGLFW); // - Directly redirect GLFW mouse button events to AntTweakBar
 	glfwSetMousePosCallback((GLFWmouseposfun)TwEventMousePosGLFW);          // - Directly redirect GLFW mouse position events to AntTweakBar
 	glfwSetMouseWheelCallback((GLFWmousewheelfun)TwEventMouseWheelGLFW);    // - Directly redirect GLFW mouse wheel events to AntTweakBar
 	glfwSetKeyCallback((GLFWkeyfun)TwEventKeyGLFW);                         // - Directly redirect GLFW key events to AntTweakBar
 	glfwSetCharCallback((GLFWcharfun)TwEventCharGLFW);                      // - Directly redirect GLFW char events to AntTweakBar
-
-
-
+ 
+ 
+ 
 	glfwSetWindowTitle( "Tutorial 17" );
-
+ 
 	// Ensure we can capture the escape key being pressed below
 	glfwEnable( GLFW_STICKY_KEYS );
 	glfwSetMousePos(1024/2, 768/2);
-
+ 
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.3f, 0.0f);
-
+ 
 	// Enable depth test
 	glEnable(GL_DEPTH_TEST);
 	// Accept fragment if it closer to the camera than the former one
 	glDepthFunc(GL_LESS); 
-
+ 
 	// Cull triangles which normal is not towards the camera
 	glEnable(GL_CULL_FACE);
-
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
+ 
 	// Create and compile our GLSL program from the shaders
 	GLuint programID = LoadShaders( "StandardShading.vertexshader", "StandardShading.fragmentshader" );
-
+ 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 	GLuint ViewMatrixID = glGetUniformLocation(programID, "V");
 	GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
-
+ 
+	// Get a handle for our buffers
+	GLuint vertexPosition_modelspaceID = glGetAttribLocation(programID, "vertexPosition_modelspace");
+	GLuint vertexUVID = glGetAttribLocation(programID, "vertexUV");
+	GLuint vertexNormal_modelspaceID = glGetAttribLocation(programID, "vertexNormal_modelspace");
+ 
 	// Load the texture
 	GLuint Texture = loadDDS("uvmap.DDS");
 	
 	// Get a handle for our "myTextureSampler" uniform
 	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
-
+ 
 	// Read our .obj file
 	std::vector<glm::vec3> vertices;
 	std::vector<glm::vec2> uvs;
 	std::vector<glm::vec3> normals;
 	bool res = loadOBJ("suzanne.obj", vertices, uvs, normals);
-
+ 
 	std::vector<unsigned short> indices;
 	std::vector<glm::vec3> indexed_vertices;
 	std::vector<glm::vec2> indexed_uvs;
 	std::vector<glm::vec3> indexed_normals;
 	indexVBO(vertices, uvs, normals, indices, indexed_vertices, indexed_uvs, indexed_normals);
-
+ 
 	// Load it into a VBO
-
+ 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, indexed_vertices.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
-
+ 
 	GLuint uvbuffer;
 	glGenBuffers(1, &uvbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glBufferData(GL_ARRAY_BUFFER, indexed_uvs.size() * sizeof(glm::vec2), &indexed_uvs[0], GL_STATIC_DRAW);
-
+ 
 	GLuint normalbuffer;
 	glGenBuffers(1, &normalbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
 	glBufferData(GL_ARRAY_BUFFER, indexed_normals.size() * sizeof(glm::vec3), &indexed_normals[0], GL_STATIC_DRAW);
-
+ 
 	// Generate a buffer for the indices as well
 	GLuint elementbuffer;
 	glGenBuffers(1, &elementbuffer);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0] , GL_STATIC_DRAW);
-
+ 
 	// Get a handle for our "LightPosition" uniform
 	glUseProgram(programID);
 	GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
-
+ 
 	// For speed computation
 	double lastTime = glfwGetTime();
+	double lastFrameTime = lastTime;
 	int nbFrames = 0;
-
+ 
 	do{
-
+ 
 		// Measure speed
 		double currentTime = glfwGetTime();
+		float deltaTime = currentTime - lastFrameTime; 
+		lastFrameTime = currentTime;
 		nbFrames++;
 		if ( currentTime - lastTime >= 1.0 ){ // If last prinf() was more than 1sec ago
 			// printf and reset
@@ -171,85 +310,89 @@ int main( void )
 			nbFrames = 0;
 			lastTime += 1.0;
 		}
-
+ 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+ 
 		// Use our shader
 		glUseProgram(programID);
-
+ 
 		glm::mat4 ProjectionMatrix = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
 		glm::mat4 ViewMatrix = glm::lookAt(
 			glm::vec3( 0, 0, 7 ), // Camera is here
 			glm::vec3( 0, 0, 0 ), // and looks here
 			glm::vec3( 0, 1, 0 )  // Head is up (set to 0,-1,0 to look upside-down)
 		);
-
+ 
 		// Bind our texture in Texture Unit 0
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, Texture);
 		// Set our "myTextureSampler" sampler to user Texture Unit 0
 		glUniform1i(TextureID, 0);
-
+ 
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
-			0,                  // attribute
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
+			vertexPosition_modelspaceID,  // The attribute we want to configure
+			3,                            // size
+			GL_FLOAT,                     // type
+			GL_FALSE,                     // normalized?
+			0,                            // stride
+			(void*)0                      // array buffer offset
 		);
-
+ 
 		// 2nd attribute buffer : UVs
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 		glVertexAttribPointer(
-			1,                                // attribute
-			2,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
+			vertexUVID,                   // The attribute we want to configure
+			2,                            // size : U+V => 2
+			GL_FLOAT,                     // type
+			GL_FALSE,                     // normalized?
+			0,                            // stride
+			(void*)0                      // array buffer offset
 		);
-
+ 
 		// 3rd attribute buffer : normals
 		glEnableVertexAttribArray(2);
 		glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
 		glVertexAttribPointer(
-			2,                                // attribute
-			3,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
+			vertexNormal_modelspaceID,    // The attribute we want to configure
+			3,                            // size
+			GL_FLOAT,                     // type
+			GL_FALSE,                     // normalized?
+			0,                            // stride
+			(void*)0                      // array buffer offset
 		);
-
+ 
 		// Index buffer
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-
+ 
 		glm::vec3 lightPos = glm::vec3(4,4,4);
 		glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
-
+ 
 		{ // Euler
-
-			glm::mat4 RotationMatrix = glm::gtx::euler_angles::eulerAngleYXZ(gEulerRotation.y, gEulerRotation.x, gEulerRotation.z);
-			glm::mat4 TranslationMatrix = glm::gtc::matrix_transform::translate(mat4(), vec3(-1.5f, 0.0f, 0.0f)); // A bit to the left
+ 
+			// As an example, rotate arount the vertical axis at 180°/sec
+			gOrientation1.y += 3.14159f/2.0f * deltaTime;
+ 
+			// Build the model matrix
+			glm::mat4 RotationMatrix = glm::gtx::euler_angles::eulerAngleYXZ(gOrientation1.y, gOrientation1.x, gOrientation1.z);
+			glm::mat4 TranslationMatrix = glm::gtc::matrix_transform::translate(mat4(), gPosition1); // A bit to the left
 			glm::mat4 ScalingMatrix = glm::gtc::matrix_transform::scale(mat4(), vec3(1.0f, 1.0f, 1.0f));
 			glm::mat4 ModelMatrix = TranslationMatrix * RotationMatrix * ScalingMatrix;
-
+ 
 			glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
-
+ 
 			// Send our transformation to the currently bound shader, 
 			// in the "MVP" uniform
 			glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 			glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
 			glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
-
-
-
+ 
+ 
+ 
 			// Draw the triangles !
 			glDrawElements(
 				GL_TRIANGLES,      // mode
@@ -257,26 +400,36 @@ int main( void )
 				GL_UNSIGNED_SHORT,   // type
 				(void*)0           // element array buffer offset
 			);
-
+ 
 		}
 		{ // Quaternion
-
-			gQuaternion = normalize(gQuaternion);
-
-			glm::mat4 RotationMatrix = glm::gtc::quaternion::mat4_cast(gQuaternion);
-			glm::mat4 TranslationMatrix = glm::gtc::matrix_transform::translate(mat4(), vec3(1.5f, 0.0f, 0.0f)); // A bit to the right
+ 
+			// It the box is checked...
+			if (gLookAtOther){
+				vec3 desiredDir = gPosition1-gPosition2;
+				vec3 desiredUp = vec3(0.0f, 1.0f, 0.0f); // +Y
+ 
+				// Compute the desired orientation
+				quat targetOrientation = normalize(LookAt(desiredDir, desiredUp));
+ 
+				// And interpolate
+				gOrientation2 = RotateTowards(gOrientation2, targetOrientation, 1.0f*deltaTime);
+			}
+ 
+			glm::mat4 RotationMatrix = glm::gtc::quaternion::mat4_cast(gOrientation2);
+			glm::mat4 TranslationMatrix = glm::gtc::matrix_transform::translate(mat4(), gPosition2); // A bit to the right
 			glm::mat4 ScalingMatrix = glm::gtc::matrix_transform::scale(mat4(), vec3(1.0f, 1.0f, 1.0f));
 			glm::mat4 ModelMatrix = TranslationMatrix * RotationMatrix * ScalingMatrix;
-
+ 
 			glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
-
+ 
 			// Send our transformation to the currently bound shader, 
 			// in the "MVP" uniform
 			glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 			glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
 			glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
-
-
+ 
+ 
 			// Draw the triangles !
 			glDrawElements(
 				GL_TRIANGLES,      // mode
@@ -285,21 +438,21 @@ int main( void )
 				(void*)0           // element array buffer offset
 			);
 		}
-
+ 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
 		glDisableVertexAttribArray(2);
-
+ 
 		// Draw GUI
 		TwDraw();
-
+ 
 		// Swap buffers
 		glfwSwapBuffers();
-
+ 
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
 		   glfwGetWindowParam( GLFW_OPENED ) );
-
+ 
 	// Cleanup VBO and shader
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteBuffers(1, &uvbuffer);
@@ -307,11 +460,11 @@ int main( void )
 	glDeleteBuffers(1, &elementbuffer);
 	glDeleteProgram(programID);
 	glDeleteTextures(1, &TextureID);
-
+ 
 	// Close GUI and OpenGL window, and terminate GLFW
 	TwTerminate();
 	glfwTerminate();
-
+ 
 	return 0;
 }
-
+ 
