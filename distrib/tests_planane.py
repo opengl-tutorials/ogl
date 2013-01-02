08296ef818855e482cd5b379fd5fccce6b1b0f61
gvtadm <gvtadm@platane.virtualys.com>
2013-01-02 20:24:30 +0100

    + Platane-specific tests : 64-bits + Optimus

---
+++ distrib/tests_planane.py
@@ -0,0 +1,22 @@
+from utils import *
+
+	
+Clean()
+
+HgUpdate21()
+PatchAll()
+Build_VC10_64()
+OptimusForceIntel()
+RunAll()
+OptimusForceNVIDIA()
+RunAll()
+
+Clean()
+
+HgUpdate33()
+PatchAll()
+Build_VC10_64()
+OptimusForceNVIDIA()
+RunAll()
+
+Clean()
---
+++ distrib/tests_platane.bat
@@ -0,0 +1,1 @@
+C:\Python27\python.exe tests_platane.py
