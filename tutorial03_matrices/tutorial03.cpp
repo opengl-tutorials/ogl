0a20f42976886ffffcccd983062dc0be994941b1
Calvin1602 <Calvin1602@localhost>
2013-04-28 17:22:20 +0200

    + Added ortho camera example

--- tutorial03_matrices/tutorial03.cpp
+++ tutorial03_matrices/tutorial03.cpp
@@ -64,6 +64,9 @@ int main( void )
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
 	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	
 	// Camera matrix
 	glm::mat4 View       = glm::lookAt(
 								glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
