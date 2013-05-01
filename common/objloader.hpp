1d3c947acc53460533f2f50b51283da4de11f659
Calvin1602 <Calvin1602@localhost>
2013-05-01 12:52:31 +0200

    + Example use of AssImp mesh loading

--- common/objloader.hpp
+++ common/objloader.hpp
@@ -8,4 +8,14 @@ bool loadOBJ(
 	std::vector<glm::vec3> & out_normals
 );
 
+
+
+bool loadAssImp(
+	const char * path, 
+	std::vector<unsigned short> & indices,
+	std::vector<glm::vec3> & vertices,
+	std::vector<glm::vec2> & uvs,
+	std::vector<glm::vec3> & normals
+);
+
 #endif
