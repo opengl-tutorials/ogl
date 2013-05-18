4ee78d2a17b203aced7e9c4aabdbd34191b17b47
Calvin1602 <Calvin1602@localhost>
2013-05-18 16:22:21 +0200

    Merge with default

--- misc05_picking/misc05_picking_BulletPhysics.cpp
+++ misc05_picking/misc05_picking_BulletPhysics.cpp
@@ -118,7 +118,7 @@ int main( void )
 	std::string message;
 	TwAddVarRW(GUI, "Last picked object", TW_TYPE_STDSTRING, &message, NULL);
 
-	glfwSetWindowTitle( "Misc 05 - Simple but slow version" );
+	glfwSetWindowTitle( "Misc 05 - Bullet version" );
 
 	// Ensure we can capture the escape key being pressed below
 	glfwEnable( GLFW_STICKY_KEYS );
@@ -141,15 +141,12 @@ int main( void )
 
 	// Create and compile our GLSL program from the shaders
 	GLuint programID = LoadShaders( "StandardShading.vertexshader", "StandardShading.fragmentshader" );
-	GLuint pickingProgramID = LoadShaders( "Picking.vertexshader", "Picking.fragmentshader" );
-
 
 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 	GLuint ViewMatrixID = glGetUniformLocation(programID, "V");
 	GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
-	GLuint PickingMatrixID = glGetUniformLocation(pickingProgramID, "MVP");
 
 	// Get a handle for our buffers
 	GLuint vertexPosition_modelspaceID = glGetAttribLocation(programID, "vertexPosition_modelspace");
@@ -209,11 +206,6 @@ int main( void )
 	}
 
 
-
-
-	// Get a handle for our "pickingColorID" uniform
-	GLuint pickingColorID = glGetUniformLocation(pickingProgramID, "PickingColor");
-
 	// Get a handle for our "LightPosition" uniform
 	glUseProgram(programID);
 	GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
@@ -244,8 +236,8 @@ int main( void )
 	std::vector<btRigidBody*> rigidbodies;
 
 	// In this example, all monkeys will use the same collision shape : 
-	// A box of 1m*1m*1m (0.5 is the half-extent !)
-	btCollisionShape* boxCollisionShape = new btBoxShape(btVector3(0.5f, 0.5f, 0.5f));
+	// A box of 2m*2m*2m (1.0 is the half-extent !)
+	btCollisionShape* boxCollisionShape = new btBoxShape(btVector3(1.0f, 1.0f, 1.0f));
 
 	for(int i=0; i<100; i++){
 
