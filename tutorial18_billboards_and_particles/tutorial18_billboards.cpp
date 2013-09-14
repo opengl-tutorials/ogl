0d75460e4fb9e9a1105dd0034d8150d310af7c35
Calvin1602 <Calvin1602@localhost>
2013-09-14 12:59:07 +0200

    - CMake : bug in shaders list + Added getchar()'s in all loaders, so that people will actually READ the error message before complaining. - Tutorial 18 : Typo, method #3, images, cube

--- tutorial18_billboards_and_particles/tutorial18_billboards.cpp
+++ tutorial18_billboards_and_particles/tutorial18_billboards.cpp
@@ -18,6 +18,8 @@ using namespace glm;
 #include <common/texture.hpp>
 #include <common/controls.hpp>
 
+#define DRAW_CUBE // Comment or uncomment this to simplify the code
+
 int main( void )
 {
 	// Initialise GLFW
@@ -76,7 +78,7 @@ int main( void )
 	GLuint CameraUp_worldspace_ID  = glGetUniformLocation(programID, "CameraUp_worldspace");
 	GLuint ViewProjMatrixID = glGetUniformLocation(programID, "VP");
 	GLuint BillboardPosID = glGetUniformLocation(programID, "BillboardPos");
-	GLuint BillboadSizeID = glGetUniformLocation(programID, "BillboadSize");
+	GLuint BillboardSizeID = glGetUniformLocation(programID, "BillboardSize");
 	GLuint LifeLevelID = glGetUniformLocation(programID, "LifeLevel");
 
 	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
@@ -94,9 +96,24 @@ int main( void )
 	GLuint billboard_vertex_buffer;
 	glGenBuffers(1, &billboard_vertex_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_DYNAMIC_DRAW);
 
-	
+#ifdef DRAW_CUBE
+	// Everything here comes from Tutorial 4
+	GLuint cubeProgramID = LoadShaders( "../tutorial04_colored_cube/TransformVertexShader.vertexshader", "../tutorial04_colored_cube/ColorFragmentShader.fragmentshader" );
+	GLuint cubeMatrixID = glGetUniformLocation(cubeProgramID, "MVP");
+	static const GLfloat g_cube_vertex_buffer_data[] = { -1.0f,-1.0f,-1.0f,-1.0f,-1.0f, 1.0f,-1.0f, 1.0f, 1.0f,1.0f, 1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f, 1.0f,-1.0f,1.0f,-1.0f, 1.0f,-1.0f,-1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f, 1.0f,-1.0f,1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f, 1.0f, 1.0f,-1.0f, 1.0f,-1.0f,1.0f,-1.0f, 1.0f,-1.0f,-1.0f, 1.0f,-1.0f,-1.0f,-1.0f,-1.0f, 1.0f, 1.0f,-1.0f,-1.0f, 1.0f,1.0f,-1.0f, 1.0f,1.0f, 1.0f, 1.0f,1.0f,-1.0f,-1.0f,1.0f, 1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f, 1.0f, 1.0f,1.0f,-1.0f, 1.0f,1.0f, 1.0f, 1.0f,1.0f, 1.0f,-1.0f,-1.0f, 1.0f,-1.0f,1.0f, 1.0f, 1.0f,-1.0f, 1.0f,-1.0f,-1.0f, 1.0f, 1.0f,1.0f, 1.0f, 1.0f,-1.0f, 1.0f, 1.0f,1.0f,-1.0f, 1.0f};
+	static const GLfloat g_cube_color_buffer_data[] = { 0.583f,  0.771f,  0.014f,0.609f,  0.115f,  0.436f,0.327f,  0.483f,  0.844f,0.822f,  0.569f,  0.201f,0.435f,  0.602f,  0.223f,0.310f,  0.747f,  0.185f,0.597f,  0.770f,  0.761f,0.559f,  0.436f,  0.730f,0.359f,  0.583f,  0.152f,0.483f,  0.596f,  0.789f,0.559f,  0.861f,  0.639f,0.195f,  0.548f,  0.859f,0.014f,  0.184f,  0.576f,0.771f,  0.328f,  0.970f,0.406f,  0.615f,  0.116f,0.676f,  0.977f,  0.133f,0.971f,  0.572f,  0.833f,0.140f,  0.616f,  0.489f,0.997f,  0.513f,  0.064f,0.945f,  0.719f,  0.592f,0.543f,  0.021f,  0.978f,0.279f,  0.317f,  0.505f,0.167f,  0.620f,  0.077f,0.347f,  0.857f,  0.137f,0.055f,  0.953f,  0.042f,0.714f,  0.505f,  0.345f,0.783f,  0.290f,  0.734f,0.722f,  0.645f,  0.174f,0.302f,  0.455f,  0.848f,0.225f,  0.587f,  0.040f,0.517f,  0.713f,  0.338f,0.053f,  0.959f,  0.120f,0.393f,  0.621f,  0.362f,0.673f,  0.211f,  0.457f,0.820f,  0.883f,  0.371f,0.982f,  0.099f,  0.879f};
+	GLuint cubevertexbuffer;
+	glGenBuffers(1, &cubevertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubevertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_vertex_buffer_data), g_cube_vertex_buffer_data, GL_DYNAMIC_DRAW);
+	GLuint cubecolorbuffer;
+	glGenBuffers(1, &cubecolorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubecolorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_color_buffer_data), g_cube_color_buffer_data, GL_DYNAMIC_DRAW);
+#endif
+
 	double lastTime = glfwGetTime();
 	do
 	{
@@ -111,6 +128,29 @@ int main( void )
 		computeMatricesFromInputs();
 		glm::mat4 ProjectionMatrix = getProjectionMatrix();
 		glm::mat4 ViewMatrix = getViewMatrix();
+
+
+
+#ifdef DRAW_CUBE
+		// Again : this is just Tutorial 4 !
+		glDisable(GL_BLEND);
+		glUseProgram(cubeProgramID);
+		glm::mat4 cubeModelMatrix(1.0f);
+		cubeModelMatrix = glm::scale(cubeModelMatrix, glm::vec3(0.2f, 0.2f, 0.2f));
+		glm::mat4 cubeMVP = ProjectionMatrix * ViewMatrix * cubeModelMatrix;
+		glUniformMatrix4fv(cubeMatrixID, 1, GL_FALSE, &cubeMVP[0][0]);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, cubevertexbuffer);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0  );
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, cubecolorbuffer);
+		glVertexAttribPointer(1, 3, GL_FLOAT,  GL_FALSE, 0, (void*)0   );
+		glDrawArrays(GL_TRIANGLES, 0, 12*3);
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+#endif
+
+
 
 		// We will need the camera's position in order to sort the particles
 		// w.r.t the camera's distance.
@@ -137,11 +177,15 @@ int main( void )
 		glUniform1i(TextureID, 0);
 
 		// This is the only interesting part of the tutorial.
+		// This is equivalent to mlutiplying (1,0,0) and (0,1,0) by inverse(ViewMatrix).
+		// ViewMatrix is orthogonal (it was made this way), 
+		// so its inverse is also its transpose, 
+		// and transposing a matrix is "free" (inversing is slooow)
 		glUniform3f(CameraRight_worldspace_ID, ViewMatrix[0][0], ViewMatrix[1][0], ViewMatrix[2][0]);
 		glUniform3f(CameraUp_worldspace_ID   , ViewMatrix[0][1], ViewMatrix[1][1], ViewMatrix[2][1]);
 		
-		glUniform3f(BillboardPosID, 0.0f, 0.0f, 0.0f); // The billboard will be at the origin
-		glUniform2f(BillboadSizeID, 1.0f, 0.125f);     // and 1m*12cm, because it matches its 256*32 resolution =)
+		glUniform3f(BillboardPosID, 0.0f, 0.5f, 0.0f); // The billboard will be just above the cube
+		glUniform2f(BillboardSizeID, 1.0f, 0.125f);     // and 1m*12cm, because it matches its 256*32 resolution =)
 
 		// Generate some fake life level and send it to glsl
 		float LifeLevel = sin(currentTime)*0.1f + 0.7f;
