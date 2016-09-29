4a46573fa4ed1381d6b37b0bf0688a2a7e212022
Calvin1602 <Arnaud1602@gmail.com>
2016-09-29 12:06:10 +0200

    fixes #22

--- common/texture.cpp
+++ common/texture.cpp
@@ -28,16 +28,18 @@ GLuint loadBMP_custom(const char * imagepath){
 	// If less than 54 bytes are read, problem
 	if ( fread(header, 1, 54, file)!=54 ){ 
 		printf("Not a correct BMP file\n");
+		fclose(file);
 		return 0;
 	}
 	// A BMP files always begins with "BM"
 	if ( header[0]!='B' || header[1]!='M' ){
 		printf("Not a correct BMP file\n");
+		fclose(file);
 		return 0;
 	}
 	// Make sure this is a 24bpp file
-	if ( *(int*)&(header[0x1E])!=0  )         {printf("Not a correct BMP file\n");    return 0;}
-	if ( *(int*)&(header[0x1C])!=24 )         {printf("Not a correct BMP file\n");    return 0;}
+	if ( *(int*)&(header[0x1E])!=0  )         {printf("Not a correct BMP file\n");    fclose(file); return 0;}
+	if ( *(int*)&(header[0x1C])!=24 )         {printf("Not a correct BMP file\n");    fclose(file); return 0;}
 
 	// Read the information about the image
 	dataPos    = *(int*)&(header[0x0A]);
@@ -70,6 +72,7 @@ GLuint loadBMP_custom(const char * imagepath){
 
 	// OpenGL has now copied the data. Free our own version
 	delete [] data;
+	fclose(file);
 
 	// Poor filtering, or ...
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
