8784b76314c752f1b0deef2cdd17396ac8801891
Foadfs <f.s.farimani@gmail.com>
2016-11-03 01:08:59 +0100

    solving the compiling issue
    this line was already in the page http://www.opengl-tutorial.org/beginners-tutorials/tutorial-1-opening-a-window/
    but for some reasons missing from the github code. adding this solves the problem

--- tutorial01_first_window/tutorial01.cpp
+++ tutorial01_first_window/tutorial01.cpp
@@ -30,6 +30,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
+	GLFWwindow* window; // (In the accompanying source code, this variable is global)
 	window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
