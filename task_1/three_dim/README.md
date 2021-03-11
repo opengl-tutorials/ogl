0c6119c0ad528f1f65cdf4496f86a23ba3ccfdda
Roman <roma_klimovitskiy@mail.ru>
2021-03-11 22:16:58 +0300

    3D model: fixed window name, added README.md

---
+++ task_1/three_dim/README.md
@@ -0,0 +1,10 @@
+# 3D model
+
+## Demonstration
+
+Click on this image to watch a demonstration on 
+[YouTube](https://www.youtube.com/watch?v=038NYlqjPmw):
+
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=erxGysNM-ko
+" target="_blank"><img src="http://img.youtube.com/vi/erxGysNM-ko/0.jpg"
+alt="3D model demonstration" width="720" height="480" border="10" /></a>
--- task_1/three_dim/three_dim.cpp
+++ task_1/three_dim/three_dim.cpp
@@ -32,7 +32,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow( 1024, 768, "Triangles", NULL, NULL);
+	window = glfwCreateWindow( 1024, 768, "3D model", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the "
                    "2.1 version of the tutorials.\n" );
